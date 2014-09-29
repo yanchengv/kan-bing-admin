383b751aa3ccf162cee7345bc08cfd0671355f61
Li Hangchao <lihangchao@outlook.com>
2014-09-29 10:06:41 +0800

    字典模块功能--级联删除省市县

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,4 @@
 class City < ActiveRecord::Base
   belongs_to(:province, :class_name => "Province", :foreign_key => "province_id")
-  has_many(:counties, :class_name => "County",:foreign_key => "city_id")
+  has_many(:counties, :class_name => "County",:foreign_key => "city_id", :dependent => :destroy)
 end
--- app/models/province.rb
+++ app/models/province.rb
@@ -1,4 +1,4 @@
 class Province < ActiveRecord::Base
-  has_many(:cities, :class_name => "City", :foreign_key => "province_id")
+  has_many(:cities, :class_name => "City", :foreign_key => "province_id", :dependent => :destroy)
   has_many(:counties, :class_name => "County", :foreign_key => "province_id")
 end
