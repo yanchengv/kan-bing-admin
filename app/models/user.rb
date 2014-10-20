92bed3c72ee18c2d41f929cd733159141133ace3
Yang Xiaofeng <yxf_cat@hotmail.com>
2014-10-20 14:39:29 +0800

    user问题修改

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
    before_create :set_pk_code
   belongs_to :doctor, :foreign_key => :doctor_id
   belongs_to :patient, :foreign_key => :patient_id
+   validates :name, presence: true, uniqueness: {case_sensitive: true, message: "该用户名已被使用！"}
+   attr_reader :password
+   has_secure_password :validations => false
   #获取登录用户收到的文章分享
    def set_pk_code
      if self.id&&self.id!=0
