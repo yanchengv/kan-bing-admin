647730f20c4518519962fae69e5513b397c0dd09
Yang Xiaofeng <yxf_cat@hotmail.com>
2015-01-16 09:27:28 +0800

    分中心医生列表显示

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -215,7 +215,7 @@ class DoctorsController < ApplicationController
   def get_doctor_to_page
     @doctor = Doctor.find(params[:id])
     if !@doctor.photo.nil? && @doctor.photo != ''
-      if aliyun_file_exit("avatar/#{@doctor.photo}" , Settings.aliyunOSS.image_bucket)
+      if aliyun_file_exit("avatar/#{@doctor.photo.strip}" , Settings.aliyunOSS.image_bucket) || aliyun_file_exit("avatar/#{@doctor.photo}", Settings.aliyunOSS.image_bucket)
         @doctor.photo = Settings.aliyunOSS.photo_url + @doctor.photo
       else
         @doctor.photo = 'default.png'
